656c4febc72574cc46bfebff67649ed1df741763
supernord <50811354+supernord@users.noreply.github.com>
2021-06-16 10:15:41 +0930

    tool list generation updates

--- tool_list_generation.R
+++ tool_list_generation.R
@@ -9,17 +9,17 @@ source("functions.R")
 ###########################
 ### 1. load matrix data ###
 ###########################
-biocommons_tool_matrix <- read_matrix("../external_GitHub_inputs/Matrix of Availability of Bioinformatics Tools across BioCommons - deployment version - Bioinformatics Software Availability.tsv")
+biocommons_tool_matrix <- read_matrix("../../external_GitHub_inputs/Matrix of Availability of Bioinformatics Tools across BioCommons - deployment version - Bioinformatics Software Availability.tsv")
 
 ###########################
 ### 2. load galaxy data ###
 ###########################
-galaxy <- parse_GA_yaml(list.files("../external_GitHub_inputs/usegalaxy.org.au/", full.names = TRUE))
+galaxy <- parse_GA_yaml(list.files("../../external_GitHub_inputs/usegalaxy.org.au/", full.names = TRUE))
 
 ##############################
 ### 3. load QRIScloud data ###
 ##############################
-qris <- read_qris("../external_GitHub_inputs/qriscloud.txt") %>%
+qris <- read_qris("../../external_GitHub_inputs/qriscloud.txt") %>%
   rename(`QRIScloud / UQ-RCC (Flashlite, Awoonga, Tinaroo)` = qris_cloud) %>%
   #see https://community.rstudio.com/t/which-tidyverse-is-the-equivalent-to-search-replace-from-spreadsheets/3548/7
   mutate_if(is.character, str_replace_all, pattern = 'genomeanalysistk', replacement = 'gatk') %>%
@@ -29,19 +29,19 @@ qris <- read_qris("../external_GitHub_inputs/qriscloud.txt") %>%
 #########################  
 ### 4. load Gadi data ###
 #########################
-gadi <- read_gadi("../external_GitHub_inputs/keys/key1.hdr") %>%
+gadi <- read_gadi("../../external_GitHub_inputs/keys/key1.hdr") %>%
   rename(`NCI (Gadi)` = version)
 
 ###########################
 ### 5. load Pawsey data ###
 ###########################
-zeus <- read_hpc("../external_GitHub_inputs/zeus.txt") %>%
+zeus <- read_hpc("../../external_GitHub_inputs/zeus.txt") %>%
   rename(`Pawsey (Zeus)` = version) %>%
   #see https://community.rstudio.com/t/which-tidyverse-is-the-equivalent-to-search-replace-from-spreadsheets/3548/7
   mutate_if(is.character, str_replace_all, pattern = 'wgs', replacement = 'celera') %>%
   mutate_if(is.character, str_replace_all, pattern = 'trinityrnaseq', replacement = 'trinity')
 
-magnus <- read_hpc("../external_GitHub_inputs/magnus.txt") %>%
+magnus <- read_hpc("../../external_GitHub_inputs/magnus.txt") %>%
   rename(`Pawsey (Magnus)` = version)
 
 #############################################################
@@ -66,9 +66,9 @@ tool_list_for_biotools <- biocommons_tool_matrix %>%
 #See https://cran.r-project.org/web/packages/tidyjson/vignettes/introduction-to-tidyjson.html
 
 biotools <- access_biotools_api_biotoolsID(tool_list_for_biotools)# %>%
-  #write_tsv("./biotools_api_metadata.tsv")
+  #write_tsv("../../external_GitHub_inputs/biotools_api_metadata.tsv")
 
-#biotools <- read_tsv("./biotools_api_metadata.tsv")
+#biotools <- read_tsv("../../external_GitHub_inputs/biotools_api_metadata.tsv")
 
 ###############################################
 ### 7. Merge bio.tools metadata with matrix ###
@@ -100,9 +100,9 @@ complete_join <- join_tool_and_install_metadata(matrix_data = merge,
 
 complete_processed <- process_tool_and_install_metadata(complete_join)
 
-complete_processed %>%
+#complete_processed %>%
   #filter(is.na(biotoolsID)) %>%
-  write_tsv(paste0("../external_GitHub_inputs/", today(), "_processed_tool_matrix.tsv"))
+#  write_tsv(paste0("../../external_GitHub_inputs/", today(), "_processed_tool_matrix.tsv"))
 
 ###other links
 #see https://stackoverflow.com/a/56683740
