3ef1fd7941df0408cf6355d5a8689045fa8fbe23
Uwe Winter <uwwint@gmail.com>
2021-11-15 08:29:26 +1100

    added module initialization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='toolfinder_dev',
+    version='0.001',
+    packages=['toolfinder'],
+    url='',
+    license='',
+    author='Johan Gustafsson',
+    author_email='johan@biocommons.org.au',
+    description='Toolfinder generation scripts'
+)
