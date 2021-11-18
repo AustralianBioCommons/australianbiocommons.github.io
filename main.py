02111d2250333a477d2ca0f6d32ee2a3eee07604
supernord <50811354+supernord@users.noreply.github.com>
2021-11-18 17:50:06 +1030

    New data providers including Galaxy and bio.tools

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from toolfinder import *
 import pandas as pd
 
 if __name__ == "__main__":
-    db = toolfinder.ToolDB("external/tool_matrix_2021_11_15.xlsx")
-    db.add_provider(ToolMatrixDataProvider("external/tool_matrix_2021_11_15.xlsx"))
-    db.add_provider(ZeusDataProvider("external/zeus.txt"))
-    db.add_provider(MagnusDataProvider("external/magnus.txt"))
-    db.add_provider(QriscloudDataProvider("external/qriscloud.txt"))
+    mytooldb = toolfinder.ToolDB("external/tool_matrix_2021_11_15.xlsx")
+    mytooldb.add_provider(ToolMatrixDataProvider("external/tool_matrix_2021_11_15.xlsx"))
+    mytooldb.add_provider(ZeusDataProvider("external/zeus.txt"))
+    mytooldb.add_provider(MagnusDataProvider("external/magnus.txt"))
+    mytooldb.add_provider(QriscloudDataProvider("external/qriscloud.txt"))
+    gdp = GalaxyDataProvider(mytooldb)
+    mytooldb.add_provider(gdp)
+
+    print("The following bio.tools IDs were not matched to the tool matrix sheet:")
+    for id in gdp.get_unmatched_biotools_ids():
+        print(id)
--- toolfinder/__init__.py
+++ toolfinder/__init__.py
@@ -1,17 +1,21 @@
 from abc import abstractmethod
 import datetime
+
+import numpy as np
 import pandas as pd
 from typing import List
-
+import requests
+import json
+import itertools
 
 class Dataprovider:
     """
-    Class represting a data source, which enriches informaiton about a tool.
+    Class representing a data source, which enriches information about a tool.
     """
 
     def __init__(self):
         self.identifier = ""
-        """available_data is keyed buy the toolid and should contain all information received by this data provider"""
+        """available_data is keyed by the toolid and should contain all information received by this data provider"""
         self.available_data = {}
         self.last_queried = datetime.datetime.min
 
@@ -20,7 +24,7 @@ class Dataprovider:
             self._query_remote()
 
     """
-    _query_remote queries a remote data source, transforms the informaiton recieved into internal identifiers to be later joined onto all available tools.
+    _query_remote queries a remote data source, transforms the information received into internal identifiers to be later joined onto all available tools.
     """
 
     @abstractmethod
@@ -60,8 +64,13 @@ class Dataprovider:
     def _render(self, data):
         pass
 
+    def get_alt_ids(self):
+        return{}
+
 
 class ToolMatrixDataProvider(Dataprovider):
+    ID_BIO_TOOLS = "bio.tools"
+
     def __init__(self, filename):
         super().__init__()
         self.filename = filename
@@ -76,6 +85,15 @@ class ToolMatrixDataProvider(Dataprovider):
 
     def _render(self, data):
         return {"show": data["include?"]}
+
+    def get_alt_ids(self):
+        if self._needs_querying():
+            self._query_remote()
+        retval = {ToolMatrixDataProvider.ID_BIO_TOOLS: {}}
+        for toolID in self.available_data:
+            row = self.available_data[toolID]
+            retval[ToolMatrixDataProvider.ID_BIO_TOOLS][row.biotoolsID] = toolID
+        return retval
 
 
 class ZeusDataProvider(Dataprovider):
@@ -95,6 +113,7 @@ class ZeusDataProvider(Dataprovider):
     def _render(self, data):
         return data
 
+
 class MagnusDataProvider(Dataprovider):
     def __init__(self, filename):
         super().__init__()
@@ -112,6 +131,7 @@ class MagnusDataProvider(Dataprovider):
     def _render(self, data):
         return data
 
+
 class QriscloudDataProvider(Dataprovider):
     def __init__(self, filename):
         super().__init__()
@@ -120,11 +140,83 @@ class QriscloudDataProvider(Dataprovider):
 
     def _query_remote(self):
         self.available_data = {}
-        data = pd.read_csv(self.filename, delimiter="/", header=None)
-        data.columns = ["toolID", "version"]
+        with open(self.filename, "r") as stream:
+            for line in stream:
+                line = line.split("/")
+                toolID = line[0].strip()
+                if len(line) == 3:
+                    toolID = toolID + "-" + line[1].strip()
+                elif len(line) > 3:
+                    raise ValueError(toolID)
+                self.available_data[toolID] = line[-1].strip()
 
-        for idx, row in data.iterrows():
-            self.available_data[row.toolID] = row.version
+    def _render(self, data):
+        return data
+
+
+class GalaxyDataProvider(Dataprovider):
+
+    def __init__(self, parent):
+        super().__init__()
+        self.identifier = "GalaxyAustralia"
+        self.parent = parent
+        self.unmatched_biotools_ids = set()
+
+    def _query_remote(self):
+        self.available_data = {}
+        req = requests.request("get", "https://usegalaxy.org.au/api/tools")
+        if req.status_code != 200:
+            raise FileNotFoundError(req.url)
+        tool_sections = json.loads(req.text)
+        #Herve Menager via Slack
+        tools_nested = [tool_section.get('elems') for tool_section in tool_sections if 'elems' in tool_section]
+        tools = list(itertools.chain.from_iterable(tools_nested))
+        tools_with_biotools = []
+        for tool in tools:
+            xref = []
+            for ref in tool.get("xrefs", []):
+                if ref["reftype"] == "bio.tools":
+                    xref.append(ref)
+            if len(xref) > 0:
+                tools_with_biotools.append(tool)
+        #tools_with_biotools = [tool for tool in tools if [xref for xref in tool.get('xrefs', []) if xref["reftype"] == "bio.tools"]]
+        #tools_with_biotools = [tool for tool in tools if filter(lambda x: x["reftype"] == "bio.tools", tool.get('xrefs', []))]
+
+        #
+        for tool in tools_with_biotools:
+            biotools_id = list(filter(lambda x: x["reftype"] == "bio.tools", tool['xrefs']))[0]['value']
+            tool_id = self.parent.get_id_from_alt(ToolMatrixDataProvider.ID_BIO_TOOLS, biotools_id)
+            if tool_id is None:
+                self.unmatched_biotools_ids.add(biotools_id)
+            self.available_data[tool_id] = tool
+
+    def get_unmatched_biotools_ids(self):
+        return self.unmatched_biotools_ids
+
+    def _render(self, data):
+        return data
+
+
+class BiotoolsDataProvider(Dataprovider):
+
+    def __init__(self, toolmatrix, parent):
+        super().__init__()
+        self.identifier = "BioTools"
+        self.filename = toolmatrix
+        self.parent = parent
+
+    def _query_remote(self):
+        self.available_data = {}
+        data = pd.read_excel(self.toolmatrix, header=2)
+        unique_biotools_ids = set(data.biotoolsID.unique())
+        unique_biotools_ids.remove(np.nan)
+        for biotools_id in unique_biotools_ids:
+            response = requests.get("https://bio.tools/api/t/?biotoolsID=%s&format=json" %biotools_id)
+            if response.status_code != 200:
+                raise FileNotFoundError(response.url)
+            tool_metadata = json.loads(response.text)
+            tool_id = self.parent.get_id_from_alt(ToolMatrixDataProvider.ID_BIO_TOOLS, biotools_id)
+            self.available_data[tool_id] = tool_metadata
 
     def _render(self, data):
         return data
@@ -165,9 +257,11 @@ class ToolDB:
         self.db = {}
         self.dataprovider: List[Dataprovider]
         self.dataprovider = []
+        self.alternateids = {}
         data = pd.read_excel(tool_matrix_file, header=2)
         for i in data.toolID:
             self.db[i] = Tool(i)
+        del self.db[np.nan]
 
     """enrich the DB with what the dataprovider has queried from its datasource"""
 
@@ -177,6 +271,17 @@ class ToolDB:
         for i in self.db:
             tool = self.db[i]
             tool.add_data(dataprovider)
+
+        alt_ids = dataprovider.get_alt_ids()
+        # https://stackoverflow.com/a/26853961 & https://www.python.org/dev/peps/pep-0584/
+        self.alternateids = {**self.alternateids, **alt_ids}
+
+    def get_id_from_alt(self, provider:str, unique_id:str):
+        if provider in self.alternateids:
+            if unique_id in self.alternateids[provider]:
+                return self.alternateids[provider][unique_id]
+        return None
+
 
     """add a dataprovider to the list of providers"""
 
