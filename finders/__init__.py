ab0c3e0bae50d348c7c70d00641bb13a7a2642a3
Johan Gustafsson <johan@biocommons.org.au>
2026-01-21 10:11:47 +1030

    remove finder code

--- finders/__init__.py
+++
@@ -1,3 +0,0 @@
-from .common import Dataprovider
-from .toolfinder import Tool, ToolDB, ToolMatrixDataProvider, QriscloudDataProvider, GadiDataProvider, if89DataProvider, GalaxyDataProvider, BiotoolsDataProvider, SetonixDataProvider, BiocontainersDataProvider
-from .workflowfinder import Workflow, WorkflowDB, WorkflowHubSpaceDataProvider
